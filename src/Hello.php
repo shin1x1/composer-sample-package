565c62e4fd356b7152f611290128427c329e14f6
shin1x1 <shin1x1@gmail.com>
2015-01-10 14:28:56 +0900

    init

---
+++ src/Hello.php
@@ -0,0 +1,18 @@
+<?php
+namespace Sample;
+
+/**
+ * Hello
+ */
+class Hello
+{
+    /**
+     * say
+     */
+    public static function say()
+    {
+        echo 'Hello!' . PHP_EOL;
+    }
+}
+
+
